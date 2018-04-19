626fd5a8c4e0d00df7f7587f9a0c5d7d5ed100d7
oktorio <oktorio@ittron-indonesia.com>
2018-04-19 10:51:56 +0700

    inisiasi awal, mockup

---
+++ admin.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cyberbrain</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+	<link href="css/landing-page.css" rel="stylesheet">
+	<link href="css/agency.css" rel="stylesheet">
+	<link href="css/carousel.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background-color:#5bc0de">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="index.php#page-top">Home</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="index.php#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="index.php#services">Services</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="index.php#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="index.php#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+					<div class="col-lg-6 col-md-6">
+						<div class="bs-example">
+						<h4>Table Data</h4>
+							<table class="table table-striped" style="border:black solid 1px">
+							  <thead>
+								<tr>
+								  <th>#</th>
+								  <th>First Name</th>
+								  <th>Last Name</th>
+								  <th>Username</th>
+								</tr>
+							  </thead>
+							  <tbody>
+								<tr>
+								  <td>1</td>
+								  <td>Mark</td>
+								  <td>Otto</td>
+								  <td>@mdo</td>
+								</tr>
+								<tr>
+								  <td>2</td>
+								  <td>Jacob</td>
+								  <td>Thornton</td>
+								  <td>@fat</td>
+								</tr>
+								<tr>
+								  <td>3</td>
+								  <td>Larry</td>
+								  <td>the Bird</td>
+								  <td>@twitter</td>
+								</tr>
+							  </tbody>
+							</table>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6">
+						<div class="bs-example">
+						<h4>Table Data</h4>
+							<table class="table table-striped" style="border:black solid 1px">
+							  <thead>
+								<tr>
+								  <th>#</th>
+								  <th>First Name</th>
+								  <th>Last Name</th>
+								  <th>Username</th>
+								</tr>
+							  </thead>
+							  <tbody>
+								<tr>
+								  <td>1</td>
+								  <td>Mark</td>
+								  <td>Otto</td>
+								  <td>@mdo</td>
+								</tr>
+								<tr>
+								  <td>2</td>
+								  <td>Jacob</td>
+								  <td>Thornton</td>
+								  <td>@fat</td>
+								</tr>
+								<tr>
+								  <td>3</td>
+								  <td>Larry</td>
+								  <td>the Bird</td>
+								  <td>@twitter</td>
+								</tr>
+							  </tbody>
+							</table>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6">
+						<div class="bs-example">
+						<h4>Table Data</h4>
+							<table class="table table-striped" style="border:black solid 1px">
+							  <thead>
+								<tr>
+								  <th>#</th>
+								  <th>First Name</th>
+								  <th>Last Name</th>
+								  <th>Username</th>
+								</tr>
+							  </thead>
+							  <tbody>
+								<tr>
+								  <td>1</td>
+								  <td>Mark</td>
+								  <td>Otto</td>
+								  <td>@mdo</td>
+								</tr>
+								<tr>
+								  <td>2</td>
+								  <td>Jacob</td>
+								  <td>Thornton</td>
+								  <td>@fat</td>
+								</tr>
+								<tr>
+								  <td>3</td>
+								  <td>Larry</td>
+								  <td>the Bird</td>
+								  <td>@twitter</td>
+								</tr>
+							  </tbody>
+							</table>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6">
+						<div class="bs-example">
+						<h4>Table Data</h4>
+							<table class="table table-hover" style="border:black solid 1px">
+							  <thead>
+								<tr>
+								  <th>Id</th>
+								  <th>Nama</th>
+								  <th>Status</th>
+								</tr>
+							  </thead>
+								<?php
+								 
+								$user_name = "root";
+								$password = "";
+								$database = "test";
+								$host_name = "localhost";
+								 
+								$connect_db=mysql_connect($host_name, $user_name, $password);
+								 
+								$find_db=mysql_select_db($database);
+								 
+								if ($find_db) {
+								 
+								 $query = "SELECT * FROM user";
+								 $hasil = mysql_query($query);
+								 
+								 while ( $kolom_db = mysql_fetch_assoc($hasil) ) {
+
+									echo "<tbody>";
+									echo "<tr>";
+										echo "<td>".$kolom_db['id']."</td>";
+										echo "<td>".$kolom_db['nama']."</td>"; 
+										echo "<td>".$kolom_db['status']."</td>"; 
+									echo "</tr>";
+									echo "</tbody>";
+								 
+								 }
+								 
+								  mysql_close($connect_db);
+								 
+								}else {
+								 
+								  echo "Database Tidak Ada";
+								 
+								  mysql_close($connect_db);
+								 
+								}
+								 
+								?>
+							</table>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+		
+		<div>
+		
+
+		
+		</div>
+		
+		
+    </section>
+	
+	
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+	<script src="js/classie.js"></script>
+	<script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+	<script src="js/agency.js"></script>
+
+
+</body>
+
+</html>
